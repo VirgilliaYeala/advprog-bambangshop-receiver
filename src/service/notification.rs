7acb2701e04d205d550e5eaafd2bf9718390a848
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-04-11 13:36:11 +0700

    feat: Implement list_messages function in Notification service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -91,4 +91,8 @@ impl NotificationService{
         return Ok(subscribe_result);
     }
 
+    pub fn list_messages() -> Result<Vec<String>> {
+        return Ok(NotificationRepository::list_all_as_string());
+    }
+
 }
